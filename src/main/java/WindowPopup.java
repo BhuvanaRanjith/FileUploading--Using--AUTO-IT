c730230ff839de9fc38dafbe8a71ec779c628ebc
Bhuvana Ranjith <mailtobhuvana04@gmail.com>
2023-06-20 14:21:57 -0700

    Initial commit

---
+++ src/main/java/WindowPopup.java
@@ -0,0 +1,28 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+@Test
+public class WindowPopup {
+
+    public void windowpopup()
+    {
+       //System.setProperty("webdriver.chrome.driver","C:\\Users\\IamBh\\IdeaProjects\\Windowpopup\\src\\Driver\\chromedriver.exe");
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        //http://username:password@siteurl
+        driver.get("http://admin:admin@the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.linkText("Basic Auth")).click();
+    }
+}
