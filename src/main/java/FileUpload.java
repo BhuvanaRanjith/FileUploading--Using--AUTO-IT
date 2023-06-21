8f1c4cd9aa191bc756f035a07fd957197c7a1cbc
Bhuvana Ranjith <mailtobhuvana04@gmail.com>
2023-06-21 14:11:22 -0700

    Automating window Controls with Selenium:
    Handling Window Authentication Pop Up
    http://Username:Password@SiteURL
    
    Driver.get();
    
    Handling File Upload from Windows using AutoIT
    
    What is AutoIT
    Install AutoIT
    AutoIT Scripting
    Integrating AutoIT with Selenium
    
    //Shift focus to the file upload windows
    
    //set text/path into file name edit box
    
    //click open to upload file
    
    Au3info- record window component objects
    
    Build Script -scite.exe
    
    Save it- .au3 extenstion
    
    Convert file into .exe by compiling .au3 file
    
    Call .exe file with Runtime class in java into your selenium tests

---
+++ src/main/java/FileUpload.java
@@ -0,0 +1,60 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+@Test
+public class FileUpload {
+
+    public void fileupload() throws InterruptedException, IOException {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("https://www.pdfsimpli.com/pdf-to-jpeg/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.xpath("//label[@id='unauthContentPlaceHolder_dragUploadPdfMain']")).click();
+        Thread.sleep(2000);
+        Runtime.getRuntime().exec("C:\\Users\\IamBh\\OneDrive\\Documents\\Check\\FileUpload.exe");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[normalize-space()=\"LET’S GET STARTED\"]")).click();
+        driver.findElement(By.id("small-business-close")).click();
+        Thread.sleep(2000);
+        WebElement Text=driver.findElement(By.xpath("//button[@title='Free Text']"));
+        Text.click();
+        Thread.sleep(2000);
+      //  WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+       // wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//button[normalize-space()='GET STARTED']"))));
+        driver.findElement(By.xpath("//div/button[@class='btn primary pull-right'][.='GET STARTED']")).click();
+
+        Actions a=new Actions(driver);
+        a.moveToElement(Text).click().build().perform();
+        a.click(Text).sendKeys("ss").build().perform();
+        driver.findElement(By.id("ConvertContinue")).click();
+
+        driver.findElement(By.id("radioJpeg")).click();
+        driver.findElement(By.xpath("//div[@class='cong-modal-desktop']/button[@name='download']")).click();
+        driver.findElement(By.id("googleBtn")).click();
+
+
+
+
+            }
+}
+//shift focus to the file upload windows
+//set text path intp file
+//click open to upload file
